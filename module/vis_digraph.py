91b575a7153e1dcee63415adae6801d8ca04777d
RektPunk <rektpunk@gmail.com>
2023-01-19 00:05:45 +0900

    feat - add streamlit run code

---
+++ module/vis_digraph.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import networkx as nx
+import plotly.graph_objects as go
+
+
+def vis_digraph(
+    df: pd.DataFrame,
+    from_: str,
+    to_: str,
+    edge_label_name: str,
+) -> go.Figure:
+    _df = df[[from_, to_, edge_label_name]]
+    _nx_network_info = nx.from_pandas_edgelist(
+        _df,
+        source=from_,
+        target=to_,
+        edge_attr=edge_label_name,
+    )
+    _node_location = nx.layout.spring_layout(_nx_network_info)
+    node_x = []
+    node_y = []
+    node_labels = []
+    for node in _nx_network_info.nodes():
+        _location_x, _location_y = _node_location[node]
+        node_labels.append(node)
+        node_x.append(_location_x)
+        node_y.append(_location_y)
+
+    node_trace = go.Scatter(
+        x=node_x,
+        y=node_y,
+        text=node_labels,
+        mode="markers+text",
+        hoverinfo="text",
+        textposition="top center",
+        marker=dict(size=10),
+        textfont = dict(
+            color="red",
+        ),
+    )
+    fig = go.Figure(
+        node_trace,
+        layout=go.Layout(
+            hovermode="closest",
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        ),
+    )
+    fig.update_layout(height = 800, width = 1800)
+    for edges in _nx_network_info.edges(data=True):
+        _location_from_x, _location_from_y = _node_location[edges[0]]
+        _location_to_x, _location_to_y = _node_location[edges[1]]
+        fig.add_annotation(
+            dict(
+                x=_location_from_x,
+                y=_location_from_y,
+                ax=_location_to_x,
+                ay=_location_to_y,
+                xref="x",
+                yref="y",
+                axref="x",
+                ayref="y",
+                showarrow=True,
+                arrowhead=1,
+                arrowsize=3,
+                arrowwidth=1,
+                arrowcolor="white",
+            )
+        )
+        fig.add_annotation(
+            dict(
+                x=(_location_from_x + _location_to_x) / 2,
+                y=(_location_from_y + _location_to_y) / 2,
+                text=edges[2][edge_label_name],
+                showarrow=True,
+                arrowhead=1,
+                arrowcolor="black",
+            )
+        )
+    return fig
---
+++ streamlit_run.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import streamlit as st
+from streamlit_tags import st_tags_sidebar
+from module.vis_digraph import vis_digraph
+
+DATA_FILENAME = "data/benddao.parquet"
+df = pd.read_parquet(DATA_FILENAME)
+
+st.write("# Bendao trace tracker")
+
+st.sidebar.write("# Input transaction hash")
+
+maxtags_sidebar = st.sidebar.slider(
+    "Number of transactions allowed?", 1, 5, 3, key="ehikwegrjifbwreuk"
+)
+
+st.session_state.transactions = st_tags_sidebar(
+    label="# Input transaction hash:",
+    text="Press enter to add more",
+    value=[],
+    maxtags=maxtags_sidebar,
+    key="afrfae",
+)
+
+st.sidebar.write("### Inputs:")
+st.sidebar.write((st.session_state.transactions))
+
+if len(st.session_state.transactions) != 0:
+    _df = df.loc[
+        df["transaction_hash"].isin(st.session_state.transactions)
+    ]
+    if len(_df) == 0:
+        st.write("Not available")
+    else:
+        _df = _df.assign(
+            _amount=[
+                str(round(int(amount) / 10**18, 3))
+                if name
+                in ["Wrapped Ether", "Bend debt bearing WETH", "Bend interest bearing WETH"]
+                else amount
+                for amount, name in zip(_df["amount"], _df["name"])
+            ]
+        )
+        _df = _df.assign(
+            edge_attr=[
+                f"{name}_{_amount}"
+                for name, _amount in zip(_df["name"], _df["_amount"])
+            ],
+        )
+        _df = _df.assign(
+            from_address_ = [address[-5:] for address in _df["from_address"]],
+            to_address_ = [address[-5:] for address in _df["to_address"]],
+        )
+        fig = vis_digraph(
+            _df,
+            "from_address_",
+            "to_address_",
+            "edge_attr",
+        )
+        st.plotly_chart(fig, height = 800, width = 1800)
