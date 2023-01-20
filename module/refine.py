321ccef798270c6692ef499cc5a01775a83783b8
RektPunk <rektpunk@gmail.com>
2023-01-20 13:17:01 +0900

    feat - change vis

---
+++ module/refine.py
@@ -0,0 +1,94 @@
+from typing import List
+import pandas as pd
+
+
+def assign_columns(
+    df: pd.DataFrame,
+    txs: List[str],
+    address_len: int = 5,
+):
+    _df = df.loc[df["transaction_hash"].isin(txs)]
+    if len(_df) == 0:
+        return None
+    _df = _df.assign(
+        _amount=[
+            str(round(int(amount) / 10**18, 3))
+            if (
+                name
+                in [
+                    "Wrapped Ether",
+                    "Aave interest bearing WETH",
+                    "Bend debt bearing WETH",
+                    "Bend interest bearing WETH",
+                ]
+                or "WETH" in name
+                or name is None
+            )
+            else amount
+            for amount, name in zip(_df["amount"], _df["name"])
+        ]
+    )
+    _df = _df.assign(
+        _amount_num=_df._amount.astype(float),
+    )
+    _df = _df.assign(
+        addresses=[
+            ",".join(sorted([from_address, to_address]))
+            for from_address, to_address in zip(_df["from_address"], _df["to_address"])
+        ],
+        from_address_=[address[-address_len:] for address in _df["from_address"]],
+        to_address_=[address[-address_len:] for address in _df["to_address"]],
+    )
+    _df = _df.assign(
+        edge_attr=[
+            f"{from_address_}->{to_address_}: {name_}_{amount_}"
+            for from_address_, to_address_, name_, amount_ in zip(
+                _df["from_address_"],
+                _df["to_address_"],
+                _df["name"],
+                _df["_amount_num"],
+            )
+        ],
+    )
+    return _df
+
+
+def get_loss_gain(
+    df,
+    address_len: int = 5,
+):
+    _loss_df = (
+        df.groupby(["from_address", "name"])
+        ._amount_num.sum()
+        .reset_index()
+        .rename(
+            columns={
+                "from_address": "address",
+                "_amount_num": "loss",
+            }
+        )
+    )
+    _gain_df = (
+        df.groupby(["to_address", "name"])
+        ._amount_num.sum()
+        .reset_index()
+        .rename(columns={"to_address": "address", "_amount_num": "gain"})
+    )
+    _balance_df = _loss_df.merge(_gain_df, how="outer", on=["address", "name"]).fillna(0)
+    _balance_df = _balance_df.assign(
+        balance_change=round(_balance_df["gain"] - _balance_df["loss"], 3)
+    )
+    _balance_df = _balance_df.loc[_balance_df["balance_change"] != 0]
+    _balance_df = _balance_df.assign(
+        address_=[address[-address_len:] for address in _balance_df["address"]],
+    )
+    return _balance_df
+
+
+def get_edge_label(df, edge_label_name="edge_attr"):
+    _edge_labels_df = (
+        df.groupby(["addresses"])[edge_label_name]
+        .apply(lambda x: "<br>".join(x))
+        .reset_index()
+    )
+    return _edge_labels_df
--- module/vis_digraph.py
+++ module/vis_digraph.py
@@ -5,31 +5,38 @@ import plotly.graph_objects as go
 
 def get_nodes(
     df: pd.DataFrame,
+    balance_df: pd.DataFrame,
     from_: str,
     to_: str,
+    _nx_network_info = None,
 ):
     _df = df[[from_, to_]]
-    _nx_network_info = nx.from_pandas_edgelist(
-        _df,
-        source=from_,
-        target=to_,
-    )
+    if _nx_network_info is None:
+        _nx_network_info = nx.from_pandas_edgelist(
+            _df,
+            source=from_,
+            target=to_,
+        )
     _node_location = nx.layout.spring_layout(_nx_network_info)
     node_x = []
     node_y = []
+    node_names = []
     node_labels = []
     for node in _nx_network_info.nodes():
+        _balance_change = balance_df.loc[balance_df["address_"] == node][["name", "balance_change"]].to_dict(orient = "records")
         _location_x, _location_y = _node_location[node]
-        node_labels.append(node)
+        node_names.append(node)
+        node_labels.append("P/L: " + '<br>'.join([_bc["name"] + " " + str(round(_bc["balance_change"], 2)) for _bc in _balance_change]))
         node_x.append(_location_x)
         node_y.append(_location_y)
 
     node_trace = go.Scatter(
-        x=node_x,
-        y=node_y,
-        text=node_labels,
-        mode="markers+text",
-        hoverinfo="text",
+        x = node_x,
+        y = node_y,
+        text = node_names,
+        mode = "markers+text",
+        hoverinfo = "text",
+        hovertext = node_labels,
         textposition="top center",
         marker=dict(
             size=10,
@@ -38,27 +45,20 @@ def get_nodes(
         textfont=dict(
             color="red",
         ),
-        name="wallet",
     )
-    return (_node_location, node_trace)
+    return (_node_location, node_trace, _nx_network_info)
 
 
 def vis_digraph(
     df: pd.DataFrame,
+    edge_label_df: pd.DataFrame,
     from_: str,
     to_: str,
-    edge_label_name: str,
     node_location,
     node_trace: go.Scatter,
     height: int,
     width: int,
 ) -> go.Figure:
-    _df = df[[from_, to_, edge_label_name]]
-    _edge_df = (
-        _df.groupby([from_, to_])[edge_label_name]
-        .apply(lambda x: ", ".join(x))
-        .reset_index()
-    )
     fig = go.Figure(
         node_trace,
         layout=go.Layout(
@@ -69,10 +69,11 @@ def vis_digraph(
         ),
     )
     fig.update_layout(height=height, width=width)
-    for _, edges in _edge_df.iterrows():
+    for _, edges in df.iterrows():
         _from = edges[from_]
         _to = edges[to_]
-        _edge_label = edges[edge_label_name]
+        _addresses = edges["addresses"]
+        _edge_attr = edge_label_df.loc[edge_label_df["addresses"] == _addresses]["edge_attr"].values[0]
         _location_from_x, _location_from_y = node_location[_from]
         _location_to_x, _location_to_y = node_location[_to]
         fig.add_annotation(
@@ -96,7 +97,7 @@ def vis_digraph(
             dict(
                 x=(_location_from_x + _location_to_x) / 2,
                 y=(_location_from_y + _location_to_y) / 2,
-                text=_edge_label,
+                text=_edge_attr,
                 font=dict(color="grey", size=12),
                 showarrow=True,
                 arrowhead=0,
