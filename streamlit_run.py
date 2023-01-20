227fc687043dc448b139c5bff5c27d27e32fffb2
RektPunk <rektpunk@gmail.com>
2023-01-20 11:20:59 +0900

    feat - streamlit

--- streamlit_run.py
+++ streamlit_run.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import streamlit as st
 from streamlit_tags import st_tags_sidebar
-from module.vis_digraph import vis_digraph
+from module.vis_digraph import get_nodes, vis_digraph
+from st_aggrid import AgGrid, GridOptionsBuilder
+
 
 DATA_FILENAME = "data/benddao.parquet"
 df = pd.read_parquet(DATA_FILENAME)
@@ -18,13 +20,9 @@ maxtags_sidebar = st.sidebar.slider(
     "Number of transactions allowed?", 1, 5, 3, key="ehikwegrjifbwreuk"
 )
 
-height = st.sidebar.slider(
-    "Height", 100, 1000, 800
-)
+height = st.sidebar.slider("Height", 100, 1000, 800)
 
-width = st.sidebar.slider(
-    "Width", 100, 1000, 800
-)
+width = st.sidebar.slider("Width", 100, 1000, 800)
 
 st.session_state.transactions = st_tags_sidebar(
     label="# Input transaction hash:",
@@ -38,9 +36,7 @@ st.sidebar.write("### Inputs:")
 st.sidebar.write((st.session_state.transactions))
 
 if len(st.session_state.transactions) != 0:
-    _df = df.loc[
-        df["transaction_hash"].isin(st.session_state.transactions)
-    ]
+    _df = df.loc[df["transaction_hash"].isin(st.session_state.transactions)]
     if len(_df) == 0:
         st.write("Not available")
     else:
@@ -48,7 +44,15 @@ if len(st.session_state.transactions) != 0:
             _amount=[
                 str(round(int(amount) / 10**18, 3))
                 if (
-                    name in ["Wrapped Ether", "Aave interest bearing WETH", "Bend debt bearing WETH", "Bend interest bearing WETH"] or "WETH" in name or name is None
+                    name
+                    in [
+                        "Wrapped Ether",
+                        "Aave interest bearing WETH",
+                        "Bend debt bearing WETH",
+                        "Bend interest bearing WETH",
+                    ]
+                    or "WETH" in name
+                    or name is None
                 )
                 else amount
                 for amount, name in zip(_df["amount"], _df["name"])
@@ -61,15 +65,66 @@ if len(st.session_state.transactions) != 0:
             ],
         )
         _df = _df.assign(
-            from_address_ = [address[-5:] for address in _df["from_address"]],
-            to_address_ = [address[-5:] for address in _df["to_address"]],
+            from_address_=[address[-5:] for address in _df["from_address"]],
+            to_address_=[address[-5:] for address in _df["to_address"]],
         )
-        fig = vis_digraph(
+        
+        _aggrid_df = _df[[
+                "from_address_", 
+                "to_address_",
+                "token_address",
+                "name",
+                "_amount",
+                "edge_attr",
+        ]]
+        grid_options_builder = GridOptionsBuilder.from_dataframe(_aggrid_df)
+        grid_options_builder.configure_pagination(
+            paginationAutoPageSize=True
+        )
+        grid_options_builder.configure_side_bar()
+        grid_options_builder.configure_selection(
+            "multiple",
+            use_checkbox=True,
+            groupSelectsChildren="Group checkbox select children",
+        )
+        gridOptions = grid_options_builder.build()
+
+        aggrid_response = AgGrid(
+            _aggrid_df,
+            gridOptions=gridOptions,
+            height=350,
+            width="100%",
+            reload_data=False,
+        )
+        _selected_df = pd.DataFrame(aggrid_response["selected_rows"])
+        
+        node_location, node_trace = get_nodes(
+            df = _df,
+            from_ = "from_address_",
+            to_ = "to_address_",
+        )
+        whole_transaction_fig = vis_digraph(
             _df,
             "from_address_",
             "to_address_",
             "edge_attr",
+            node_location,
+            node_trace,
             height,
             width,
         )
-        st.plotly_chart(fig, height = height, width = width)
+        st.plotly_chart(whole_transaction_fig, height=height, width=width)
+        if len(_selected_df) != 0:
+            selected_transaction_fig = vis_digraph(
+                _selected_df,
+                "from_address_",
+                "to_address_",
+                "edge_attr",
+                node_location,
+                node_trace,
+                height,
+                width,
+            )
+            st.plotly_chart(selected_transaction_fig, height=height, width=width)
+        
+        
